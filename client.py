6f7a90105efb3d8f746b65eec83ccf6782147ab1
MunaBurshaid-202102871 <154453480+MunaBurshaid-202102871@users.noreply.github.com>
2023-12-30 23:32:31 +0300

    The code of client

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ from tkinter import messagebox, simpledialog
 
 # Server's information
 S_address = '127.0.0.1'
-S_port = 1356
+S_port = 1373
 
 # Connect the client to the server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,16 +15,19 @@ client_socket.connect((S_address, S_port))
 Username = simpledialog.askstring("Username", "Enter your username: ")
 client_socket.send(Username.encode('ascii'))
 
-# Create a function to send requests to the server
-def send_request(request_type):
-    # Send the request type to the server
-    client_socket.send(request_type.encode('ascii'))
+# Create a function to handle response
+def handle_response(request_type, response):
+    # Print the raw response for debugging
+    print("Raw Response:", repr(response))
 
-    # Receive the response from the server
-    response = client_socket.recv(4096).decode('ascii')
+    try:
+        flights = json.loads(response)
+    except json.JSONDecodeError as e:
+        # Print the exception for debugging
+        print("JSON Decode Error:", e)
+        messagebox.showerror('Error', 'Error decoding server response.')
+        return
 
-    # Process and display the flight information
-    flights = json.loads(response)
     if not flights:
         messagebox.showinfo('No Flights', 'No flights found. SORRY!')
     else:
@@ -32,50 +35,76 @@ def send_request(request_type):
         for flight in flights:
             if request_type.lower() == 'a':
                 # Arrived flights
-                flight_info += f"Flight Code (IATA): {flight['flight']['iata']}\n"
-                flight_info += f"Departure Airport: {flight['departure']['airport']}\n"
-                flight_info += f"Arrival Time: {flight['arrival']['estimated']}\n"
-                flight_info += f"Arrival Terminal: {flight['arrival']['terminal']}\n"
-                flight_info += f"Arrival Gate: {flight['arrival']['gate']}\n\n"
+                flight_info += f"Flight Code (IATA): {flight['flight_code']}\n"
+                flight_info += f"Departure Airport: {flight['departure_airport']}\n"
+                flight_info += f"Arrival Time: {flight['arrival_time']}\n"
+                flight_info += f"Arrival Terminal: {flight['arrival_terminal']}\n"
+                flight_info += f"Arrival Gate: {flight['arrival_gate']}\n\n"
 
             elif request_type.lower() == 'b':
                 # Delayed flights
-                flight_info += f"Flight Code (IATA): {flight['flight']['iata']}\n"
-                flight_info += f"Departure Airport: {flight['departure']['airport']}\n"
-                flight_info += f"Departure Time: {flight['departure']['estimated']}\n"
-                flight_info += f"Estimated Arrival Time: {flight['arrival']['estimated']}\n"
-                flight_info += f"Delay: {flight['arrival']['delay']}\n"
-                flight_info += f"Terminal: {flight['arrival']['terminal']}\n"
-                flight_info += f"Gate: {flight['arrival']['gate']}\n\n"
+                flight_info += f"Flight Code (IATA): {flight['flight_code']}\n"
+                flight_info += f"Departure Airport: {flight['departure_airport']}\n"
+                flight_info += f"Departure Time: {flight['original_departure_time']}\n"
+                flight_info += f"Estimated Arrival Time: {flight['estimated_time_arrival']}\n"
+                flight_info += f"Delay: {flight['delay']}\n"
+                flight_info += f"Terminal: {flight['arrival_terminal']}\n"
+                flight_info += f"Gate: {flight['arrival_gate']}\n\n"
 
             elif request_type.lower() == 'c':
                 # All flights coming from a specific city
-                c_iata = simpledialog.askstring("City IATA", "Enter city IATA: ")
-                client_socket.send(c_iata.encode('ascii'))
-                flight_info += f"Flight Code (IATA): {flight['flight']['iata']}\n"
-                flight_info += f"Departure Airport: {flight['departure']['airport']}\n"
-                flight_info += f"Departure Time: {flight['departure']['estimated']}\n"
-                flight_info += f"Estimated Arrival Time: {flight['arrival']['estimated']}\n"
-                flight_info += f"Departure Gate: {flight['departure']['gate']}\n"
-                flight_info += f"Arrival Gate: {flight['arrival']['gate']}\n"
-                flight_info += f"Status: {flight['flight']['status']}\n\n"
+                city_iata = simpledialog.askstring("City IATA", "Enter city IATA: ")
+                if city_iata:
+                    send_request(f"c{city_iata}")
+
+                    flight_info += f"Flight Code (IATA): {flight['flight']['iata']}\n"
+                    flight_info += f"Departure Airport: {flight['departure']['airport']}\n"
+                    flight_info += f"Departure Time: {flight['departure']['estimated']}\n"
+                    flight_info += f"Estimated Arrival Time: {flight['arrival']['estimated']}\n"
+                    flight_info += f"Departure Gate: {flight['departure']['gate']}\n"
+                    flight_info += f"Arrival Gate: {flight['arrival']['gate']}\n"
+                    flight_info += f"Status: {flight['flight']['status']}\n\n"
 
             elif request_type.lower() == 'd':
                 # Details of a particular flight
-                f_iata = simpledialog.askstring("Flight IATA", "Enter flight IATA: ")
-                client_socket.send(f_iata.encode('ascii'))
-                flight_info += f"Flight Code (IATA): {flight['flight']['iata']}\n"
-                flight_info += f"Departure Airport: {flight['departure']['airport']}\n"
-                flight_info += f"Departure Gate: {flight['departure']['gate']}\n"
-                flight_info += f"Departure Terminal: {flight['departure']['terminal']}\n"
-                flight_info += f"Arrival Airport: {flight['arrival']['airport']}\n"
-                flight_info += f"Arrival Gate: {flight['arrival']['gate']}\n"
-                flight_info += f"Arrival Terminal: {flight['arrival']['terminal']}\n"
-                flight_info += f"Status: {flight['flight']['status']}\n"
-                flight_info += f"Scheduled Departure Time: {flight['departure']['scheduled']}\n"
-                flight_info += f"Scheduled Arrival Time: {flight['arrival']['scheduled']}\n\n"
+                flight_iata = simpledialog.askstring("Flight IATA", "Enter flight IATA: ")
+                if flight_iata:
+                    send_request(f"d{flight_iata}")
+
+                    flight_info += f"Flight Code (IATA): {flight['flight']['iata']}\n"
+                    flight_info += f"Departure Airport: {flight['departure']['airport']}\n"
+                    flight_info += f"Departure Gate: {flight['departure']['gate']}\n"
+                    flight_info += f"Departure Terminal: {flight['departure']['terminal']}\n"
+                    flight_info += f"Arrival Airport: {flight['arrival']['airport']}\n"
+                    flight_info += f"Arrival Gate: {flight['arrival']['gate']}\n"
+                    flight_info += f"Arrival Terminal: {flight['arrival']['terminal']}\n"
+                    flight_info += f"Status: {flight['flight']['status']}\n"
+                    flight_info += f"Scheduled Departure Time: {flight['departure']['scheduled']}\n"
+                    flight_info += f"Scheduled Arrival Time: {flight['arrival']['scheduled']}\n\n"
 
         messagebox.showinfo('Flight Information', flight_info)
+
+# Create a function to send requests to the server
+def send_request(request_type, data=None):
+    if request_type.lower() not in ('c', 'd'):
+        # Send the request type to the server
+        client_socket.send(request_type.encode('ascii'))
+
+        # Receive the response from the server
+        response = ""
+        while True:
+            chunk = client_socket.recv(4096).decode('ascii')
+            if not chunk:
+                break
+            response += chunk
+            try:
+                json.loads(response)  # Attempt to parse the JSON
+                break  # Break if JSON parsing succeeds
+            except json.JSONDecodeError:
+                continue  # Continue receiving until a complete JSON is received
+
+        # Handle the response
+        handle_response(request_type, response)
 
 # Create a function to handle quitting
 def quit():
@@ -91,7 +120,7 @@ window = tk.Tk()
 window.title("Flight Information Client")
 
 # Create a menu
-menu= tk.Menu(window)
+menu = tk.Menu(window)
 window.config(menu=menu)
 
 # Create a submenu for request types
